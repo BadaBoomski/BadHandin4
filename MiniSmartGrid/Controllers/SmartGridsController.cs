c2a4d20ab3dc49b1679d5d285c77ea068d56f388
BadaBoomski <20119144@post.au.dk>
2018-12-06 09:53:28 +0100

    SmartGridsController now updated to using repository

--- MiniSmartGrid/Controllers/SmartGridsController.cs
+++ MiniSmartGrid/Controllers/SmartGridsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MiniSmartGrid.Interfaces;
 using MiniSmartGrid.Models;
 
 namespace MiniSmartGrid.Controllers
@@ -14,29 +15,38 @@ namespace MiniSmartGrid.Controllers
     public class SmartGridsController : ControllerBase
     {
         private readonly SmartGridDBContext _context;
+        private readonly ISmartGridRepo _smartgridRepo;
 
-        public SmartGridsController(SmartGridDBContext context)
+        public SmartGridsController(ISmartGridRepo smartGridRepo)
         {
-            _context = context;
+            _smartgridRepo = smartGridRepo;
         }
+
+        //public SmartGridsController(SmartGridDBContext context)
+        //{
+        //    _context = context;
+        //}
 
         // GET: api/SmartGrids
         [HttpGet]
         public IEnumerable<SmartGrid> GetSmartGrids()
         {
-            return _context.SmartGrids;
+            return _smartgridRepo.GetAll();
+            //return _context.SmartGrids;
         }
 
         // GET: api/SmartGrids/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetSmartGrid([FromRoute] int id)
+        public IActionResult GetSmartGrid([FromRoute] int id)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var smartGrid = await _context.SmartGrids.FindAsync(id);
+
+            var smartGrid = _smartgridRepo.GetByID(id);
+            //var smartGrid = await _context.SmartGrids.FindAsync(id);
 
             if (smartGrid == null)
             {
@@ -48,7 +58,7 @@ namespace MiniSmartGrid.Controllers
 
         // PUT: api/SmartGrids/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutSmartGrid([FromRoute] int id, [FromBody] SmartGrid smartGrid)
+        public IActionResult PutSmartGrid([FromRoute] int id, [FromBody] SmartGrid smartGrid)
         {
             if (!ModelState.IsValid)
             {
@@ -60,66 +70,75 @@ namespace MiniSmartGrid.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(smartGrid).State = EntityState.Modified;
+            _smartgridRepo.UpdateSmartGrid(smartGrid);
+            _smartgridRepo.Save();
+            //_context.Entry(smartGrid).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SmartGridExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!SmartGridExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
 
-            return NoContent();
+            return Ok(smartGrid);
         }
 
         // POST: api/SmartGrids
         [HttpPost]
-        public async Task<IActionResult> PostSmartGrid([FromBody] SmartGrid smartGrid)
+        public IActionResult PostSmartGrid([FromBody] SmartGrid smartGrid)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            _context.SmartGrids.Add(smartGrid);
-            await _context.SaveChangesAsync();
+            _smartgridRepo.InsertSmartGrid(smartGrid);
+            _smartgridRepo.Save();
+
+            //_context.SmartGrids.Add(smartGrid);
+            //await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetSmartGrid", new { id = smartGrid.SmartGridID }, smartGrid);
         }
 
         // DELETE: api/SmartGrids/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteSmartGrid([FromRoute] int id)
+        public IActionResult DeleteSmartGrid([FromRoute] int id)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var smartGrid = await _context.SmartGrids.FindAsync(id);
+            var smartGrid = _smartgridRepo.GetByID(id);
+
+            //var smartGrid = await _context.SmartGrids.FindAsync(id);
             if (smartGrid == null)
             {
                 return NotFound();
             }
 
-            _context.SmartGrids.Remove(smartGrid);
-            await _context.SaveChangesAsync();
+            _smartgridRepo.RemoveSmartGrid(id);
+            _smartgridRepo.Save();
+            //_context.SmartGrids.Remove(smartGrid);
+            //await _context.SaveChangesAsync();
 
             return Ok(smartGrid);
         }
 
-        private bool SmartGridExists(int id)
-        {
-            return _context.SmartGrids.Any(e => e.SmartGridID == id);
-        }
+        //private bool SmartGridExists(int id)
+        //{
+        //    return _context.SmartGrids.Any(e => e.SmartGridID == id);
+        //}
     }
 }
