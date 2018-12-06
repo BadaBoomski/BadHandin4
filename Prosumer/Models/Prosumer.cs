60403fc60ce915d49e2e6c67bdec29a8535c0b45
Rasmus Brask Rolsted <au563135@uni.au.dk>
2018-12-06 11:36:51 +0100

    ændret prosumer class lidt

--- Prosumer/Models/Prosumer.cs
+++ Prosumer/Models/Prosumer.cs
@@ -8,9 +8,12 @@ namespace Prosumer.Models
     public class Prosumer
     {
         public int ProsumerID { get; set; }
-        public int Comsumed { get; set; }
-        public int Produced { get; set; }
-        public int TheDifferents { get; set; }
+        public int ExpectedComsumed { get; set; }
+        public int ExpectedProduced { get; set; }
+        public int CurrentComsumed { get; set; }
+        public int CurrentProduced { get; set; }
+        public int ActualComsumed { get; set; }
+        public int ActualProduced { get; set; }
         public string Type { get; set; }
 
 
