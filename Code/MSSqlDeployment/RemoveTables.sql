489c6edc220b4b0bf6785554a85d6f4ce48c9923
Parweiz <parweizhaqshenas@hotmail.com>
2018-12-13 18:45:25 +0100

    Deployed to st server

--- Code/MSSqlDeployment/RemoveTables.sql
+++ Code/MSSqlDeployment/RemoveTables.sql
@@ -1,6 +1,6 @@
-﻿ALTER TABLE [dbo].[ProsumerSmartMeterRecordsLogs] DROP CONSTRAINT [FK_ProsumerSmartMeterRecordsLogs_Prosumers_ProsumerID];
+﻿--ALTER TABLE [dbo].[ProsumerSmartMeterRecordsLogs] DROP CONSTRAINT [FK_ProsumerSmartMeterRecordsLogs_Prosumers_ProsumerID];
 DROP TABLE [dbo].[__EFMigrationsHistory];
-DROP TABLE [dbo].[Prosumers];
-DROP TABLE [dbo].[ProsumerSmartMeterRecordsLogs];
+--DROP TABLE [dbo].[Prosumers];
+--DROP TABLE [dbo].[ProsumerSmartMeterRecordsLogs];
 DROP TABLE [dbo].[SmartGridProsumers];
 DROP TABLE [dbo].[SmartGrids];
